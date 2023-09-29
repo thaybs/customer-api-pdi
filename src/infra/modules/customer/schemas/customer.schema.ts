53de3ad9ad8526f01186a340e98e80f4877f44ff
thay-bento <thayna.silva@stone.com.br>
2023-09-29 00:17:57 -0300

    fix: remove indexes parameters of payload

--- src/infra/modules/customer/schemas/customer.schema.ts
+++ src/infra/modules/customer/schemas/customer.schema.ts
@@ -1,14 +1,27 @@
 import * as mongoose from 'mongoose'
 import { AddressSchema } from './address.schema'
 
-export const CustomerSchema = new mongoose.Schema({
-  id: String,
-  name: { type: String, required: true },
-  document: { type: String, required: true },
-  email: { type: String, required: false },
-  phone: { type: String, required: false },
-  address: AddressSchema,
-  active: { type: Boolean, required: true },
-  createdAt: { type: Date, required: false },
-  deletedAt: { type: Date, required: false },
-})
+export const CustomerSchema = new mongoose.Schema(
+  {
+    id: String,
+    name: { type: String, required: true },
+    document: { type: String, required: true },
+    email: { type: String, required: false },
+    phone: { type: String, required: false },
+    address: AddressSchema,
+    active: { type: Boolean, required: true },
+    createdAt: { type: Date, required: false },
+    deletedAt: { type: Date, required: false },
+  },
+  {
+    // Excluir _id e __v de todas as consultas
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: (doc, ret) => {
+        delete ret._id
+        delete ret.__v
+      },
+    },
+  },
+)
