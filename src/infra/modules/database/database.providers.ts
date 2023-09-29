41eb61a1b4e7651e640d0c87057dd5605e9839db
thay-bento <thayna.silva@stone.com.br>
2023-09-28 23:20:48 -0300

    feat: set environment variable

--- src/infra/modules/database/database.providers.ts
+++ src/infra/modules/database/database.providers.ts
@@ -1,12 +1,11 @@
 import * as mongoose from 'mongoose'
+import 'dotenv/config'
+
 import { DATABASE_CONNECTION } from 'src/infra/crosscutting/constants'
 
 export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION,
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        'mongodb+srv://customer-api:customerpass0@cluster0.n0wxypo.mongodb.net/?retryWrites=true&w=majority',
-      ),
+    useFactory: (): Promise<typeof mongoose> => mongoose.connect(process.env.MONGO_URL),
   },
 ]
