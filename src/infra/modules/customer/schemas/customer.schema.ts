e8e6c0ba892efad112e7cc60c755215b4456d164
thay-bento <thayna.silva@stone.com.br>
2023-10-21 17:12:35 -0300

    refactor: customer schemas

--- src/infra/modules/customer/schemas/customer.schema.ts
+++ src/infra/modules/customer/schemas/customer.schema.ts
@@ -1,27 +1,34 @@
 import * as mongoose from 'mongoose'
-import { AddressSchema } from './address.schema'
+import { Schema } from 'mongoose'
 
 export const CustomerSchema = new mongoose.Schema(
   {
-    id: String,
+    id: { type: String, primary: true, generated: 'uuid' },
     name: { type: String, required: true },
     document: { type: String, required: true },
-    email: { type: String, required: false },
-    phone: { type: String, required: false },
-    address: AddressSchema,
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: [
+      new Schema(
+        {
+          postalCode: { type: String, required: false },
+          street: { type: String, required: false },
+          number: { type: String, required: false },
+          neighborhood: { type: String, required: false },
+          city: { type: String, required: false },
+        },
+        {
+          _id: false,
+        },
+      ),
+    ],
     active: { type: Boolean, required: true },
     createdAt: { type: Date, required: false },
     deletedAt: { type: Date, required: false },
+    updatedAt: { type: Date, required: false },
   },
   {
-    // Excluir _id e __v de todas as consultas
-    toJSON: {
-      virtuals: true,
-      versionKey: false,
-      transform: (doc, ret) => {
-        delete ret._id
-        delete ret.__v
-      },
-    },
+    versionKey: false,
+    timestamps: true,
   },
 )
