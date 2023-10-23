d04f066865a2e06329ed3c594f328932027923f0
thay-bento <thayna.silva@stone.com.br>
2023-10-22 22:19:07 -0300

    test: implement authorization endpoint

---
+++ test/auth/auth.service.spec.ts
@@ -0,0 +1,29 @@
+import { AuthService } from 'src/auth/auth.service'
+import { ForbiddenException } from '@nestjs/common'
+import { sign } from 'jsonwebtoken'
+
+describe('AuthService', () => {
+  let authService: AuthService
+
+  beforeEach(() => {
+    authService = new AuthService()
+  })
+
+  describe('verifyToken', () => {
+    it('should throw ForbiddenException if token is not provided', () => {
+      expect(() => authService.verifyToken(undefined)).toThrowError(ForbiddenException)
+    })
+
+    it('should throw ForbiddenException if token is invalid', () => {
+      const invalidToken = 'invalid.token'
+
+      expect(() => authService.verifyToken(invalidToken)).toThrowError(ForbiddenException)
+    })
+
+    it('should throw ForbiddenException if token has expired', () => {
+      const expiredToken = 'your_expired_token_here' // Replace with an expired token
+
+      expect(() => authService.verifyToken(expiredToken)).toThrowError(ForbiddenException)
+    })
+  })
+})
