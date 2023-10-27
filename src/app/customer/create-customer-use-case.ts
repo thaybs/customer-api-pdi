e2cd9664abe998b365c5b1a90e565f9e94afa23b
thay-bento <thayna.silva@stone.com.br>
2023-10-27 18:11:06 -0300

    fix: adjust usecases with new validation

--- src/app/customer/create-customer-use-case.ts
+++ src/app/customer/create-customer-use-case.ts
@@ -1,22 +1,15 @@
 import { Injectable, Post, Body, Inject, PreconditionFailedException } from '@nestjs/common'
 import { Model } from 'mongoose'
-import { ICreateCustomerParams } from 'src/api/customer/ICreateCustomer'
-import { ICustomer } from 'src/domain/Customer'
+import { CreateCustomerParams, CreateCustomerResponse } from 'src/api/customer/ICreateCustomer'
 import { CUSTOMER_MODEL } from 'src/infra/crosscutting/constants'
-import CustomerService from 'src/infra/modules/customer/customer.service'
-import { createCustomerSchema } from './schemas/create-customer-schema'
+import { Customer } from 'src/domain/Customer'
 
 @Injectable()
 export class CreateCustomerUseCase {
-  constructor(@Inject(CUSTOMER_MODEL) private readonly customerModel: Model<ICustomer>) {}
-  execute(params: ICreateCustomerParams): Promise<ICustomer> {
-    try {
-      const validatedData = createCustomerSchema.parse(params)
-      const createdCustomer = new this.customerModel(validatedData)
+  constructor(@Inject(CUSTOMER_MODEL) private readonly customerModel: Model<Customer>) {}
+  execute(params: CreateCustomerParams): Promise<CreateCustomerResponse> {
+    const createdCustomer = new this.customerModel(params)
 
-      return createdCustomer.save()
-    } catch (error) {
-      throw new PreconditionFailedException()
-    }
+    return createdCustomer.save()
   }
 }
