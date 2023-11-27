49cdadfa064c83dbdb43bc13e46fffa6cb6d4d25
thay-bento <thayna.silva@stone.com.br>
2023-11-27 15:02:03 -0300

    feat: create customer repository

--- src/app/customer/list-customers-use-case.ts
+++ src/app/customer/list-customers-use-case.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { FilterQuery, Model } from 'mongoose'
-import { ListCustomerDto, ListCustomerResponse } from 'src/api/customer/list-customer-dto'
+import { Model } from 'mongoose'
+import { ListCustomerDto, ListCustomerResponse } from 'src/api/customer/dto/list-customer-dto'
 import { Customer } from 'src/domain/customer/entities/Customer'
+import { CustomerRepository } from 'src/infra/data/model/customer.repository'
 import { CustomerDocument } from 'src/infra/modules/database/mongoose/customer/schema/customer.schema'
 import MongooseRepository from 'src/infra/modules/database/mongoose/mongoose.repository'
 
@@ -9,7 +10,7 @@ import MongooseRepository from 'src/infra/modules/database/mongoose/mongoose.rep
 export class ListCustomerUseCase {
   constructor(
     private mongooseRepository: MongooseRepository<CustomerDocument>,
-    private readonly model: Model<CustomerDocument>,
+    private customerRepository: CustomerRepository,
   ) {}
 
   async execute(params: ListCustomerDto): Promise<ListCustomerResponse> {
@@ -17,21 +18,11 @@ export class ListCustomerUseCase {
 
     let filteredCustomers: Customer[]
 
-    if (filter && filter.name) {
-      filteredCustomers = await this.findAllByNamePartialMatch(params)
+    if (filter) {
+      filteredCustomers = await this.customerRepository.findAllByNamePartialMatch(params)
     } else {
       filteredCustomers = await this.mongooseRepository.findAllWithPaginationAndFilters(filter, page, pageSize)
     }
-
     return { page, pageSize, data: filteredCustomers }
-  }
-
-  async findAllByNamePartialMatch(params: ListCustomerDto): Promise<Customer[]> {
-    const { filter, page, pageSize } = params
-
-    const skip = (page - 1) * pageSize
-    const query = { name: { $regex: filter?.name, $options: 'i' }, document: filter?.document }
-
-    return this.model.find(query).skip(skip).limit(pageSize).exec()
   }
 }
