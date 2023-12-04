9864fece615f0d307f677138960d1215d3535a46
thay-bento <thayna.silva@stone.com.br>
2023-12-03 23:31:22 -0300

    feat: update log message

--- src/infra/crosscutting/constants.ts
+++ src/infra/crosscutting/constants.ts
@@ -1,11 +1,3 @@
-export const MESSAGES = {
-  APPLICATION_START: `Starting Nest application...`,
-  APPLICATION_READY: `Nest application successfully started`,
-
-  UNKNOWN_EXCEPTION_MESSAGE: 'Internal server error',
-  ERROR_DURING_SHUTDOWN: 'Error happened during shutdown',
-}
-
 export const DATABASE_CONNECTION = 'DATABASE_CONNECTION'
 export const CUSTOMER_MODEL = 'CUSTOMER_MODEL'
 export const CUSTOMER_NAME = 'Customer'
