a9d3fc6e84401e3a3cd1080f43bade72c6160f3b
thay-bento <thayna.silva@stone.com.br>
2023-12-07 16:56:53 -0300

    test: create customer repository

--- test/src/app/delete-customer-by-id-use-case.spec.ts
+++ test/src/app/delete-customer-by-id-use-case.spec.ts
@@ -1,13 +1,13 @@
-import { Model } from 'mongoose'
-import { CustomerDocument } from 'src/infra/modules/database/mongoose/customer/schema/customer.schema'
-import MongooseRepository from 'src/infra/modules/database/mongoose/mongoose.repository'
 import { DeleteCustomerByIdUseCase } from 'src/app/customer/delete-customer-by-id-use-case'
 import { NotFoundException } from '@nestjs/common'
 import { mockCustomer, idMockCustomer } from '../infra/customer/customer.mock'
+import CustomerRepository from 'src/infra/modules/customer/customer.repository'
+import { Model } from 'mongoose'
+import { CustomerDocument } from 'src/infra/modules/database/mongoose/customer/schema/customer.schema'
 
 describe('DeleteCustomerByIdUseCase', () => {
   let deleteCustomerByIdUseCase: DeleteCustomerByIdUseCase
-  let mongooseRepository: MongooseRepository<CustomerDocument>
+  let customerRepository: CustomerRepository
 
   beforeEach(() => {
     const customerModel: Model<CustomerDocument> = {
@@ -15,23 +15,23 @@ describe('DeleteCustomerByIdUseCase', () => {
       delete: jest.fn(),
     } as unknown as Model<CustomerDocument>
 
-    mongooseRepository = new MongooseRepository(customerModel)
+    customerRepository = new CustomerRepository(customerModel)
 
-    deleteCustomerByIdUseCase = new DeleteCustomerByIdUseCase(mongooseRepository)
+    deleteCustomerByIdUseCase = new DeleteCustomerByIdUseCase(customerRepository)
   })
 
   it('should delete a customer by its id', async () => {
-    jest.spyOn(mongooseRepository, 'findOne').mockResolvedValue(mockCustomer as CustomerDocument)
-    jest.spyOn(mongooseRepository, 'delete').mockResolvedValue()
+    jest.spyOn(customerRepository, 'findOne').mockResolvedValue(mockCustomer as CustomerDocument)
+    jest.spyOn(customerRepository, 'delete').mockResolvedValue()
 
     const customer = await deleteCustomerByIdUseCase.execute(idMockCustomer)
 
-    expect(mongooseRepository.findOne).toBeCalled()
+    expect(customerRepository.findOne).toBeCalled()
     expect(customer).toBeUndefined()
   })
 
   it('should throw an error if the customer was not found', async () => {
-    jest.spyOn(mongooseRepository, 'findOne').mockResolvedValue(null)
+    jest.spyOn(customerRepository, 'findOne').mockResolvedValue(null)
 
     try {
       await deleteCustomerByIdUseCase.execute(idMockCustomer)
--- test/src/app/get-customer-by-id-use-case.spec.ts
+++ test/src/app/get-customer-by-id-use-case.spec.ts
@@ -1,35 +1,35 @@
 import { Model } from 'mongoose'
 import { CustomerDocument } from 'src/infra/modules/database/mongoose/customer/schema/customer.schema'
-import MongooseRepository from 'src/infra/modules/database/mongoose/mongoose.repository'
 import { GetCustomerByIdUseCase } from 'src/app/customer/get-customer-by-id-use-case'
 import { NotFoundException } from '@nestjs/common'
 import { mockCustomer, idMockCustomer } from '../infra/customer/customer.mock'
+import CustomerRepository from 'src/infra/modules/customer/customer.repository'
 
 describe('GetCustomerByIdUseCase', () => {
   let getCustomerByIdUseCase: GetCustomerByIdUseCase
-  let mongooseRepository: MongooseRepository<CustomerDocument>
+  let customerRepository: CustomerRepository
 
   beforeEach(() => {
     const customerModel: Model<CustomerDocument> = {
       findOne: jest.fn(),
     } as unknown as Model<CustomerDocument>
 
-    mongooseRepository = new MongooseRepository(customerModel)
+    customerRepository = new CustomerRepository(customerModel)
 
-    getCustomerByIdUseCase = new GetCustomerByIdUseCase(mongooseRepository)
+    getCustomerByIdUseCase = new GetCustomerByIdUseCase(customerRepository)
   })
 
   it('should get a customer by its id', async () => {
-    jest.spyOn(mongooseRepository, 'findOne').mockResolvedValue(mockCustomer as CustomerDocument)
+    jest.spyOn(customerRepository, 'findOne').mockResolvedValue(mockCustomer as CustomerDocument)
 
     const customer = await getCustomerByIdUseCase.execute(idMockCustomer)
 
-    expect(mongooseRepository.findOne).toBeCalled()
+    expect(customerRepository.findOne).toBeCalled()
     expect(customer).toBe(mockCustomer)
   })
 
   it('should throw an error if the customer was not found', async () => {
-    jest.spyOn(mongooseRepository, 'findOne').mockResolvedValue(null)
+    jest.spyOn(customerRepository, 'findOne').mockResolvedValue(null)
 
     try {
       await getCustomerByIdUseCase.execute(idMockCustomer)
